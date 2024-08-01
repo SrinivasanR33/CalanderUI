83a8f3896d9daba7ff9580e5f4b904da1a90ca3d
Srinivasan <vasan4649@gmail.com>
2024-08-01 22:56:59 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port:8000,
+    proxy:{
+      "/api":{
+        target:'http://52.35.66.255:8000',
+        changeOrigin:true,
+        rewrite:(path)=>path.replace(/^\/api/,"/api")
+      }
+    }
+  }
 })
